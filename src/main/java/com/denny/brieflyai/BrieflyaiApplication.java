ea1fe7e065c10322a2e286a8118f7dc4c11f7079
Denny Mathew <165308292+DennyM55@users.noreply.github.com>
2026-09-04 14:06:59 +0530

    Initial commit

---
+++ src/main/java/com/denny/brieflyai/BrieflyaiApplication.java
@@ -0,0 +1,13 @@
+package com.denny.brieflyai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrieflyaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrieflyaiApplication.class, args);
+	}
+
+}
